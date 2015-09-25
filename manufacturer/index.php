fd455978680500d9757e385b63f042c870b4911e
pTk <ptk609@gmail.com>
2015-09-25 15:02:36 +0530

    Added Manufacturer index page

---
+++ manufacturer/index.php
@@ -0,0 +1,6 @@
+<form action="../include/login.php" method="post">
+Username : <input type="text" name="mfgUname" /><br>
+Password : <input type="password" name="mfgPass" /><br>
+<input type="hidden" name="loginType" value="mfg" />
+<input type="submit" value="Login" />
+</form>
